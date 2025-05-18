ba1c321d13041cf1dac7c0e6d5f93b9bbf2c2362
Elhoucine Elmourabit <elhoucine.elmourabit.pro@gmail.com>
2025-05-18 10:39:12 +0100

    [projet] : ajout de la couche DTO

---
+++ project-spring-angular-jwt/backend/src/main/java/com/example/backend/dtos/CurrentAccountDTO.java
@@ -0,0 +1,17 @@
+package com.example.backend.dtos;
+
+
+import com.example.backend.entities.enums.AccountStatus;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class CurrentAccountDTO extends BankAccountDTO {
+    private String id;
+    private double balance;
+    private Date createdAt;
+    private AccountStatus status;
+    private CustomerDTO customerDTO;
+    private double overDraft;
+}
